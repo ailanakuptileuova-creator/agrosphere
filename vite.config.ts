82170546c08ef14b8771c0e241948241f849df6b
Adiya-32 <adiya.khasan@mail.ru>
2026-03-01 03:32:38 +0500

    feat: enable tailwind v4 plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; // <--- Добавили это
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // <--- И это
+  ],
   server: {
     proxy: {
-      // Это перенаправит запросы с http://localhost:5173/api 
-      // на ваш сервер http://localhost:3000/api
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
